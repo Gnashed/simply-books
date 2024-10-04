1ba0c76613427bdc9c6a3df0c4450494b41fff8b
Tion Blackmon <tion.blackmon@outlook.com>
2024-10-04 08:24:56 -0500

    Books on Sale data is now rendering.

--- src/app/book/sale/page.jsx
+++ src/app/book/sale/page.jsx
@@ -1,5 +1,26 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import BookCard from '../../../components/BookCard';
+import { booksOnSale } from '../../../api/bookData';
 
 export default function BooksOnSalePage() {
-  return <div>BooksOnSalePage</div>;
+  const [books, SetBooks] = useState([]);
+
+  const getAllBooksOnSale = () => {
+    booksOnSale().then(SetBooks);
+  };
+
+  useEffect(() => {
+    getAllBooksOnSale();
+  }, []);
+
+  return (
+    <>
+      <h1>Books on Sale</h1>
+      {books.map((obj) => (
+        <BookCard bookObj={obj} onUpdate={getAllBooksOnSale} />
+      ))}
+    </>
+  );
 }
