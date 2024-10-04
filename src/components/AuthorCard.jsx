36f5095daefa862f886feef1af1cf7e7bde2e947
Tion Blackmon <tion.blackmon@outlook.com>
2024-10-03 21:07:54 -0500

    Added Next.js links for the View and Edit buttons on each author card.

--- src/components/AuthorCard.jsx
+++ src/components/AuthorCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, Button } from 'react-bootstrap';
 import PropTypes from 'prop-types';
+import Link from 'next/link';
 
 export default function AuthorCard({ authorObj }) {
   return (
@@ -16,8 +17,13 @@ export default function AuthorCard({ authorObj }) {
         </Card.Title>
         <hr />
         <Card.Text>{authorObj.email}</Card.Text>
-        <Button variant="success">View</Button>
-        <Button variant="primary">Edit</Button>
+        <Link href={`/authors/${authorObj.firebaseKey}`} passHref>
+          <Button variant="success">View</Button>
+        </Link>
+        <Link href={`/authors/edit/${authorObj.firebaseKey}`} passHref>
+          <Button variant="primary">Edit</Button>
+        </Link>
+
         <Button variant="danger">Delete</Button>
       </Card.Body>
     </Card>
