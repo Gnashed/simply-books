a96cb5f5c9c2aa7c47f742ea1fc8d5c8c75ebd7a
Tion Blackmon <tion.blackmon@outlook.com>
2024-10-04 23:15:39 -0500

    Heart icon dynamically renders if the author is marked as a favorite in Firebase.

--- src/app/authors/[firebaseKey]/page.jsx
+++ src/app/authors/[firebaseKey]/page.jsx
@@ -20,6 +20,13 @@ export default function ViewAuthor({ params }) {
       <h1>Author</h1>
       <h4>
         {authorDetails.first_name} {authorDetails.last_name}
+        {authorDetails.favorite === true ? (
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-heart-fill" viewBox="0 0 16 16">
+            <path fillRule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314" />
+          </svg>
+        ) : (
+          ''
+        )}
       </h4>
       <p>{authorDetails.email}</p>
       <div>
