bbafac4e0bf1ff9f773ccbe1646e1b0488d686ef
Tion Blackmon <tion.blackmon@outlook.com>
2024-10-07 21:02:05 -0500

    Getting the search box to fully function is still a WIP. So far I'm using state inside of the SearchBox component that keeps track of what the user type in. I can also console.lo the data and see it in real time.

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,9 +1,27 @@
-// import React, { useState } from 'react';
-import React from 'react';
-// import searchBooks from '../api/bookData';
+'use client';
+
+import React, { useState } from 'react';
+// import { searchBooks } from '../api/bookData';
 
 export default function SearchBox() {
+  const [formInputs, SetFormInputs] = useState('');
+
+  const handleChange = (e) => {
+    // Destructure name and value from the event object, target it.
+    const { name, value } = e.target;
+    // Update the state of formInputs. Pass in a param inside the callback. That param represent the previous state.
+    SetFormInputs((prevState) => ({
+      // We're combining the previous keystrokes with the next keystroke entered by the user.
+      ...prevState,
+      [name]: value,
+    }));
+    // console.log(name, value);
+    console.log(formInputs);
+  };
+
   return (
-    <input className="mr-sm-2" id="search-box" placeholder="Search book titles" aria-label="Search" />
+    <form>
+      <input className="mt-2" id="search-box" name="search-box" placeholder="Search book titles" aria-label="Search" style={{ height: '1.40rem' }} onChange={handleChange} />
+    </form>
   );
 }
